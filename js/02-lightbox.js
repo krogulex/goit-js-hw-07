b38be66d16fb8f204ad4d244e18b067011e2c016
Maciej Krogulski <maciejkrogulski1@gmail.com>
2022-10-06 14:24:08 +0200

    update lightbox 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+const galleryCreate = 
+galleryItems
+.map( (item) =>
+
+`<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+)
+.join('')
+
+gallery.innerHTML = galleryCreate
+
+/*
+    galleryItemsNode[index].addEventListener('click', (event) => {
+    event.preventDefault();
+    const galleryItemLink = galleryItemsNode[index].querySelector('a').href
+*/
+let lightbox = new SimpleLightbox('.gallery a', { 
+    /* options */ 
+
+});
